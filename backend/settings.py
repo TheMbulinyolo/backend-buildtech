08a263ee3ce567f9172ea6812f57f2a67bd34b9f
EDDY MBL <thembledpro@gmai.com>
2025-05-07 08:08:40 +0200

    Mis à jour destin

--- backend/settings.py
+++ backend/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'destin-db',
-        'USER': 'postgres',
-        'PASSWORD': 'TheVie_243',
-        'HOST': 'webuy-db.clusccmo099o.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
